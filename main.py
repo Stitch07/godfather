9ee62d5b51889d4c13d705345bc231c14cde490d
Soumil <soumil.paranjpay@gmail.com>
2020-06-14 16:16:46 +0530

    add some more helper methods

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ config = json.load(open('config.json'))
 bot = commands.Bot(command_prefix='=')
 
 if __name__ == '__main__':
-    print('Hello')
     for extension in ['misc']:
         bot.load_extension(extension)
-    print(bot.extensions)
 
 bot.run(config.get('token'))
