45f1eaa65e6729b1f6033a2b0468c14c3f9762fa
Soumil <soumil.paranjpay@gmail.com>
2020-06-20 19:55:19 +0530

    dynanic role imports

--- roles/__init__.py
+++ roles/__init__.py
@@ -1,20 +1,15 @@
-from .super_saint import *
-from .vanilla import *
-from .goon import *
-from .vigilante import *
-from .doctor import *
-from .cop import *
-from .jester import *
-from .serial_killer import *
+from pathlib import Path
+from importlib import import_module
 from .role import Role
 
-all_roles = {
-    'super_saint': SuperSaint,
-    'vanilla': Vanilla,
-    'goon': Goon,
-    'vig': Vigilante,
-    'doctor': Doctor,
-    'cop': Cop,
-    'jester': Jester,
-    'serial_killer': SerialKiller
-}
+all_roles = {}
+
+
+def filter_func(p: Path):
+    return p.is_file() and p.name not in ['roles.py', '__init__.py']
+
+
+for file in filter(filter_func, Path('roles/').iterdir()):
+    role_mod = import_module(f'roles.{file.stem}')
+    cls_name = ''.join(map(str.title, file.stem.split('_')))
+    all_roles[file.stem] = getattr(role_mod, cls_name)
