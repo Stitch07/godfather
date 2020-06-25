c185eb0c4b58a535a9721eeabeb4a13dd847235e
AriusX7 <icyligii@gmail.com>
2020-06-25 22:49:57 +0530

    Use `secrets.SystemRandom` instead of random.org

--- utils/rng.py
+++ utils/rng.py
@@ -1,11 +1,13 @@
 import random
-import requests
+import secrets
 
 
 def get_random_sequence(low: int, high: int):
-    """Generate (pseudo-)random number sequence of all numbers in 
-    a closed interval. Uses random.org, falls back to standard
-    random.sample() if an error occurs.
+    """Generate (pseudo-)random number sequence of all numbers in a closed interval.
+
+    Uses `secrets.SystemRandom` to generate cryptographically secure pseudo-random number
+    sequence. If a source of randomness can't be accessed from the operating system,
+    `random` module is used to generate a pseudo-random number sequence.
 
     Arguments:
         low: int, high: int
@@ -15,18 +17,18 @@ def get_random_sequence(low: int, high: int):
         (eg: low <= number <= high).
 
     Return Type:
-        list[int]"""
+        list[int]
+    """
 
-    # Send HTTP request to random.org
-    resp = requests.get('https://www.random.org/sequences/'
-                        f'?min={low}&max={high}&col=1&'
-                        'format=plain&rnd=new')
-
-    # Check if status is OK
-    # If so, read through each line, form a sequence by turning
-    # everything into an int, and return it
-    if resp.status_code == 200:
-        return list(map(int, resp.iter_lines()))
-
-    # If an error is shown, fallback to standard random
-    return random.sample(range(low, high + 1), high - low + 1)
+    # Use `secrets.SystemRandom()` to generate random sample.
+    # `secrets.SystemRandom` is the same as `random.SystemRandom`
+    # but methods in `random` module use `random.Random` instead of
+    # `randon.SystemRandom`.
+    # On some operating systems, the source of randomness may not be accessible
+    # or even exist. We need to fallback to the random module in such cases.
+    try:
+        # We need to use an instance of `SystemRandom` as `secrets` doesn't have a
+        # high level `sample` function.
+        return secrets.SystemRandom().sample(range(low, high + 1), high - low + 1)
+    except NotImplementedError:
+        return random.sample(range(low, high + 1), high - low + 1)
