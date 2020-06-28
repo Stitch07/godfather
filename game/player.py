a450b894844b4f69ea08b8dc8cf7870e331e06b4
melodicht <marvincht@gmail.com>
2020-06-28 15:20:46 +0800

    Add minor refactor
    `game` parameter removed from the role_pm function,
    which was completely unnecessary.

--- game/player.py
+++ game/player.py
@@ -16,7 +16,7 @@ class Player:
         self.death_reason = ''
 
     # generates the role's PM
-    def role_pm(self, game):
+    def role_pm(self):
         return (f'Hello {self.user}, you are a **{self.full_role}**. {self.role.description}'
                 f'\nWin Condition: {self.faction.win_con}')
 
