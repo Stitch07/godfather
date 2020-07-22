e9e5600592d11e16c3a2c7ac64e30d5c36f6fc9c
Radiating8 <theradiatingone@gmail.com>
2020-07-22 00:37:28 -0400

    docstring fix

--- godfather/roles/mafia/vanilla.py
+++ godfather/roles/mafia/vanilla.py
@@ -5,9 +5,11 @@ DESCRIPTION = 'You have no powers.'
 
 class VanillaMafia(MafiaMember, Role):
     """
-    A regular mafia without any powers.
+    A regular mafia without any special powers.
 
     - Win Condition: Kill anyone who will not submit to the mafia.
+
+    Abilities:
     + If the Goon dies, you will be given the final say over nightkill selection.
     """
     name = 'Vanilla Mafia'
