b2cac79f3c54554eb2fe522fe0f6da2b5938b942
Radiating8 <theradiatingone@gmail.com>
2020-08-08 17:01:44 -0400

    fix compile-time typing errors

--- src/inhibitors/mafiaChecks.ts
+++ src/inhibitors/mafiaChecks.ts
@@ -9,7 +9,7 @@ export default class extends Inhibitor {
 		if (command.gameOnly && !game) throw 'A game of Mafia is not running in this channel.';
 		if (command.gameStartedOnly && !game!.hasStarted) throw 'The game hasn\'t started yet!';
 		if (command.playerOnly && !game!.players.get(msg.author)) throw 'This command is player-only.';
-		if (command.hostOnly && game!.host !== msg.author) throw 'This command is host-only.';
+		if (command.hostOnly && game!.host.user !== msg.author) throw 'This command is host-only.';
 
 		if (command.alivePlayerOnly) {
 			const player = game!.players.get(msg.author);
