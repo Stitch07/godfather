cd7fb2e9c965de739c833c24d78a7f3762d54556
Soumil <soumil.paranjpay@gmail.com>
2020-08-09 11:59:04 +0530

    patch(votes): fix invalid player errors

--- src/commands/Mafia/vote.ts
+++ src/commands/Mafia/vote.ts
@@ -7,7 +7,7 @@ import Player from '@mafia/Player';
 @ApplyOptions<GodfatherCommandOptions>({
 	aliases: ['vtl', 'vt'],
 	description: 'Vote to lynch a player',
-	usage: '[player:player]',
+	usage: '<target:player>',
 	gameOnly: true,
 	gameStartedOnly: true,
 	playerOnly: true,
@@ -17,8 +17,10 @@ export default class extends GodfatherCommand {
 
 	public async run(msg: KlasaMessage, [target]: [Player]) {
 		const { game } = msg.channel as GodfatherChannel;
+
 		const voter = game!.players.get(msg.author)!;
 		const hammered = game!.votes.vote(voter, target);
+
 		await (msg.channel as GodfatherChannel).sendMessage(`Voted ${target.user.tag}.`);
 		if (hammered) {
 			return (msg.channel as GodfatherChannel).sendMessage('Hammered?');
