47017a30d509580406be93b0f76217a44030a967
Soumil <soumil.paranjpay@gmail.com>
2020-08-16 12:35:53 +0530

    chore: update license to AGPL

--- __main__.py
+++ __main__.py
@@ -1,17 +1,18 @@
-# This file is part of Godfather.
+# This file is part of Godfather
+# Copyright (C) 2020 Soumil07
 
-# Godfather is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 
-# Godfather is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with Godfather.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from datetime import datetime
 import json
