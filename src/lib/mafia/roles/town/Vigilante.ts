714815269679088fe718ee4f647a7163474681d8
Radiating8 <theradiatingone@gmail.com>
2020-10-17 14:09:55 -0500

    Partial implementation of vig guilt, todo make noaction work and write custom guilt message

--- src/lib/mafia/roles/town/Vigilante.ts
+++ src/lib/mafia/roles/town/Vigilante.ts
@@ -1,18 +1,48 @@
 import Shooter from '@mafia/mixins/Shooter';
-import NightActionsManager from '@mafia/managers/NightActionsManager';
+import NightActionsManager, { NightActionPriority } from '@mafia/managers/NightActionsManager';
 import Townie from '@mafia/mixins/Townie';
 import Player from '@mafia/Player';
+import { Message } from 'discord.js';
 
 class Vigilante extends Shooter {
 
 	public name = 'Vigilante';
 	public description = 'You may shoot someone every night.';
+	private guilt = false;
+
+	public async onNight() {
+		if (this.guilt) {
+			this.game.nightActions.addAction({
+				action: this.action,
+				actor: this.player,
+				target: this.player,
+				priority: NightActionPriority.VIGI_SUICIDE,
+				flags: this.flags
+			});
+
+			await this.player.user.send('You cannot shoot tonight due to guilt.');
+		} else {
+			return super.onNight();
+		}
+	}
+
+	public async onPmCommand(message: Message, command: string, ...args: string[]) {
+		if (this.guilt) {
+			return this.player.user.send('You cannot shoot tonight due to guilt.');
+		} else {
+			return super.onPmCommand(message, command, args[0]);
+		}
+	}
 
 	public tearDown(actions: NightActionsManager, target: Player) {
 		const record = actions.record.get(target.user.id).get('nightkill');
 		const success = record.result && record.by.includes(this.player);
 		if (!success) {
 			return this.player.user.send('Your target was too strong to kill!');
+		}
+
+		if (target.role.innocence) {
+			this.guilt = true;
 		}
 		return target.user.send('You were shot by a Vigilante. You have died!');
 	}
