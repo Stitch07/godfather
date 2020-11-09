c3c30b6f539f6d3e2e92d5cc7863f5d3aeabb0f9
Soumil <soumil.paranjpay@gmail.com>
2020-11-09 17:12:45 +0530

    fix(setupinfo): sort setups by totalPlayers

--- src/preconditions/AdminOnly.ts
+++ src/preconditions/AdminOnly.ts
@@ -5,8 +5,6 @@ export default class extends Precondition {
 
 	public run(message: Message) {
 		if (message.guild && message.member!.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) return this.ok();
-		// TODO: remove this later
-		if (message.author.id === this.client.ownerID) return this.ok();
 		return this.error(this.name, 'This command can only be used by server admins.');
 	}
 
