1c1b142f29e6667bb8aa809d366f42f6806ca7a3
Soumil <soumil.paranjpay@gmail.com>
2020-11-21 18:21:52 +0530

    feat: add reboot command, pm2 ecosystem file

---
+++ src/commands/System/reboot.ts
@@ -0,0 +1,18 @@
+import { CommandOptions } from '@sapphire/framework';
+import { ApplyOptions } from '@sapphire/decorators';
+import GodfatherCommand from '@lib/GodfatherCommand';
+import { Message } from 'discord.js';
+
+@ApplyOptions<CommandOptions>({
+	preconditions: ['OwnerOnly']
+})
+export default class extends GodfatherCommand {
+
+	public async run(message: Message) {
+		// when used with pm2/Docker/another process manager, it will restart the bot as soon as
+		// it shuts down
+		await message.channel.send('Rebooting...');
+		return process.exit(0);
+	}
+
+}
