5ccee7984b11175282646b8c22d0d3ea93099eff
Soumil <soumil.paranjpay@gmail.com>
2020-12-23 21:04:19 +0530

    fix: small changes

--- src/config.example.ts
+++ src/config.example.ts
@@ -1,5 +1,5 @@
 // #region bot token
-export const TOKEN = process.env.TOKEN ? process.env.TOKEN : 'YOUR_BOT_TOKEN_HERE';
+export const TOKEN = process.env.TOKEN ?? 'YOUR_BOT_TOKEN_HERE';
 // #endregion
 
 export const CLIENT_ID = '';
@@ -12,11 +12,11 @@ export const PREFIX = PRODUCTION ? '=' : 'g!';
 
 export const PGSQL_ENABLED = false;
 export const PGSQL_DATABASE_NAME = 'godfather';
-export const PGSQL_DATABASE_PASSWORD = process.env.PGSQL_DATABASE_PASSWORD ? process.env.PGSQL_DATABASE_PASSWORD : '';
-export const PGSQL_DATABASE_USER = process.env.PGSQL_DATABASE_USER ? process.env.PGSQL_DATABASE_USER : '';
-export const PGSQL_DATABASE_PORT = 5432;
-export const PGSQL_DATABASE_HOST = process.env.PGSQL_DATABASE_HOST ? process.env.PGSQL_DATABASE_HOST : 'localhost';
+export const PGSQL_DATABASE_PASSWORD = process.env.PGSQL_DATABASE_PASSWORD ?? '';
+export const PGSQL_DATABASE_USER = process.env.PGSQL_DATABASE_USER ?? 'postgres';
+export const PGSQL_DATABASE_PORT = process.env.PGSQL_DATABASE_EXPOSED_PORT ?? 5432;
+export const PGSQL_DATABASE_HOST = process.env.PGSQL_DATABASE_HOST ?? 'localhost';
 
-export const PRIVATE_CHANNEL_SERVER = process.env.PRIVATE_CHANNEL_SERVER ? process.env.PRIVATE_CHANNEL_SERVER : '';
+export const PRIVATE_CHANNEL_SERVER = process.env.PRIVATE_CHANNEL_SERVER ?? '732226178085027900';
 export const PRIVATE_CHANNEL_CATEGORY: string | null = null;
 export const ENABLE_PRIVATE_CHANNELS = false;
