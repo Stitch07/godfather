29f54cdfb5a2febd654be6ce81fce180aa8ee339
Jurien Hamaker <jurien@kings-dev.io>
2020-12-23 10:53:31 +0100

    feat: Add docker development process
    - Dockerfile for dev & prod
    - Docker-compose for dev & prod
    - Make use of .env configuration
    - Add scripts for ease of use

--- src/config.example.ts
+++ src/config.example.ts
@@ -1,8 +1,8 @@
 // #region bot token
-export const TOKEN = 'YOUR_BOT_TOKEN_HERE';
+export const TOKEN = process.env.TOKEN ? process.env.TOKEN : 'YOUR_BOT_TOKEN_HERE';
 // #endregion
 
-export const CLIENT_ID = 'YOUR_CLIENT_ID_HERE';
+export const CLIENT_ID = '';
 
 export const SUPPORT_SERVER = 'https://discord.gg/gFhvChy';
 
@@ -12,11 +12,11 @@ export const PREFIX = PRODUCTION ? '=' : 'g!';
 
 export const PGSQL_ENABLED = false;
 export const PGSQL_DATABASE_NAME = 'godfather';
-export const PGSQL_DATABASE_PASSWORD = '';
-export const PGSQL_DATABASE_USER = '';
+export const PGSQL_DATABASE_PASSWORD = process.env.PGSQL_DATABASE_PASSWORD ? process.env.PGSQL_DATABASE_PASSWORD : '';
+export const PGSQL_DATABASE_USER = process.env.PGSQL_DATABASE_USER ? process.env.PGSQL_DATABASE_USER : '';
 export const PGSQL_DATABASE_PORT = 5432;
-export const PGSQL_DATABASE_HOST = 'localhost';
+export const PGSQL_DATABASE_HOST = process.env.PGSQL_DATABASE_HOST ? process.env.PGSQL_DATABASE_HOST : 'localhost';
 
-export const PRIVATE_CHANNEL_SERVER = '';
+export const PRIVATE_CHANNEL_SERVER = process.env.PRIVATE_CHANNEL_SERVER ? process.env.PRIVATE_CHANNEL_SERVER : '';
 export const PRIVATE_CHANNEL_CATEGORY: string | null = null;
 export const ENABLE_PRIVATE_CHANNELS = false;
