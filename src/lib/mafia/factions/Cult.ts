4881cd7118e8c5df8dcead9a63e8baf6f3262aae
MeDerp <zachw001234@gmail.com>
2021-01-17 08:12:00 -0600

    cult faction (no roles)

---
+++ src/lib/mafia/factions/Cult.ts
@@ -0,0 +1,19 @@
+import Faction from '@mafia/structures/Faction';
+import Game from '../structures/Game';
+
+const SUPPORTING_FACTIONS = ['Witch', 'Survivor', 'Cult'];
+
+export default class CultFaction extends Faction {
+
+	public name = 'Cult';
+	public winCondition = 'Convert everyone to your side.';
+	public informed = true;
+
+	public hasWon(game: Game) {
+		const aliveOpposing = game.players.filter(player => player.isAlive && !SUPPORTING_FACTIONS.includes(player.role.faction.name)).length;
+		const aliveFactional = game.players.filter(player => player.isAlive && player.role.faction.name === 'Cult').length;
+
+		return aliveOpposing === 0 && aliveFactional > 0;
+	}
+
+}
