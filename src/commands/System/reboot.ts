3d1ee4662faf4ad57e77a4a1b1dcbfa67bfa89b9
Soumil07 <soumildiscord@gmail.com>
2021-01-28 09:12:07 +0530

    refactor: use Collection#values instead

--- src/commands/System/reboot.ts
+++ src/commands/System/reboot.ts
@@ -10,9 +10,9 @@ export default class extends GodfatherCommand {
 	public async run(message: Message) {
 		// when used with pm2/Docker/another process manager, it will restart the bot as soon as
 		// it shuts down
-		for (const g of this.context.client.games) {
-			await g[1].delete();
-			await g[1].channel.send('The game has been deleted due to a bot shutdown, sorry!');
+		for (const game of this.context.client.games.values()) {
+			await game.delete();
+			await game.channel.send('The game has been deleted due to a bot shutdown, sorry!');
 		}
 		await message.channel.send('Rebooting...');
 		return process.exit(0);
