e489e0ed6d4edbd759ed6189fee1310df901d57a
MeDerp <zachw001234@gmail.com>
2021-02-02 10:15:28 -0600

    refactor: minor changes
    idk if refactor is the right word for this

--- ormconfig.js
+++ ormconfig.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-module.exports = require('./dist/lib/orm/ormConfig').config;
+export default require('./dist/lib/orm/ormConfig').config;
