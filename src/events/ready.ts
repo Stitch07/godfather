4824f647b046ab77df73d5af31039f10d74d1c08
Soumil <soumildiscord@gmail.com>
2021-02-14 16:08:56 +0530

    refactor: remove slashCommand support

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-negated-condition */
 import { connect } from '@lib/orm/ormConfig';
-import { CLIENT_ID, PGSQL_ENABLED, PREFIX, SENTRY_DSN } from '@root/config';
-import { Event, Events, isErr, PieceContext, Store } from '@sapphire/framework';
+import { PGSQL_ENABLED, PREFIX, SENTRY_DSN } from '@root/config';
+import { Event, Events, PieceContext, Store } from '@sapphire/framework';
 import * as Sentry from '@sentry/node';
 import { floatPromise } from '@util/utils';
 import { gray, green, magenta, yellow } from 'colorette';
@@ -49,41 +49,8 @@ export default class extends Event<Events.Ready> {
 			}
 		}, 10 * 1000); // 10 seconds
 
-		for (const command of this.context.client.stores.get('slashCommands').values()) {
-			// @ts-ignore more private calls until d.js supports interactions
-			await this.context.client.api.applications(CLIENT_ID).commands.post({
-				data: {
-					name: command.name,
-					description: command.description
-				}
-			});
-		}
-
 		this.logReady();
 		await this.context.client.uploadBotStats();
-
-		// @ts-ignore d.js needs to be updated first
-		this.context.client.ws.on('INTERACTION_CREATE', async (interaction) => {
-			const command = this.context.client.stores.get('slashCommands').get(interaction.data.name);
-			if (!command) return null;
-
-			const fauxMessage = {
-				channel: this.context.client.channels.cache.get(interaction.channel_id)!,
-				guild: this.context.client.channels.cache.get(interaction.channel_id)!
-			};
-			// @ts-ignore more hacks with faux messages
-			const result = await command.preconditions.run(fauxMessage, command);
-			if (isErr(result) && result.error.message !== '') {
-				return command.reply(interaction, result.error.message);
-			}
-
-			try {
-				await command.run(interaction);
-			} catch (error) {
-				if (typeof error === 'string') return command.reply(interaction, error);
-				this.context.client.logger.error(error);
-			}
-		});
 	}
 
 	private logReady() {
