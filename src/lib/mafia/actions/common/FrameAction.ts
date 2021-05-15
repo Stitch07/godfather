901456c4ea32c25bd904565e31d54b3b7b42af8e
Radiating8 <theradiatingone@gmail.com>
2021-05-15 19:21:01 -0400

    Refactor framer

---
+++ src/lib/mafia/actions/common/FrameAction.ts
@@ -0,0 +1,20 @@
+import type Player from '@mafia/structures/Player';
+import NightActionsManager, { NightActionPriority } from '../../managers/NightActionsManager';
+import type { ActionRole } from '../../structures/ActionRole';
+import { SingleTargetAction } from '../mixins/SingleTargetAction';
+
+export class FrameAction extends SingleTargetAction {
+	public name = 'frame';
+	public priority = NightActionPriority.Frame;
+
+	public constructor(role: ActionRole) {
+		super(role);
+
+		this.actionText = this.game.t('roles/actions:frameText');
+		this.actionGerund = this.game.t('roles/actions:frameGerund');
+	}
+
+	public setUp(actions: NightActionsManager, target: Player) {
+		actions.framedPlayers.push(target);
+	}
+}
--- src/lib/mafia/roles/mafia/Framer.ts
+++ src/lib/mafia/roles/mafia/Framer.ts
@@ -1,22 +1,16 @@
-import NightActionsManager, { NightActionPriority } from '@mafia/managers/NightActionsManager';
 import MafiaRole from '@mafia/mixins/MafiaRole';
 import SingleTarget from '@mafia/mixins/SingleTarget';
 import type Player from '@mafia/structures/Player';
+import { FrameAction } from '../../actions/common/FrameAction';
+import type { NightAction } from '../../managers/NightAction';
 
 class Framer extends SingleTarget {
 	public name = 'Framer';
-	public action = 'frame';
-	public priority = NightActionPriority.Frame;
+	public actions: NightAction[] = [new FrameAction(this)];
 
 	public constructor(player: Player) {
 		super(player);
 		this.description = this.game.t('roles/neutral:framerDescription');
-		this.actionText = this.game.t('roles/actions:framerText');
-		this.actionGerund = this.game.t('roles/actions:framerGerund');
-	}
-
-	public setUp(actions: NightActionsManager, target: Player) {
-		actions.framedPlayers.push(target);
 	}
 }
 
