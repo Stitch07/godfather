4ff0f6d858e665741bc8a58bcc13565414cd4a08
Radiating8 <theradiatingone@gmail.com>
2021-05-15 18:09:02 -0400

    Refactor neapolitan

---
+++ src/lib/mafia/actions/common/NeapolitanAction.ts
@@ -0,0 +1,24 @@
+import type Player from '@mafia/structures/Player';
+import NightActionsManager, { NightActionPriority } from '../../managers/NightActionsManager';
+import type { ActionRole } from '../../structures/ActionRole';
+import { SingleTargetAction } from '../mixins/SingleTargetAction';
+
+const VANILLA_ROLES = ['Cult Member', 'Vanilla', 'Vanilla Mafia'];
+
+export class NeapolitanAction extends SingleTargetAction {
+	public name = 'check';
+	public priority = NightActionPriority.Investigative;
+	public constructor(role: ActionRole) {
+		super(role);
+		this.actionText = this.game.t('roles/actions:neapolitanText');
+		this.actionGerund = this.game.t('roles/actions:neapolitanGerund');
+	}
+
+	public tearDown(actions: NightActionsManager, target: Player) {
+		if (VANILLA_ROLES.includes(target.role.name)) {
+			this.player.queueMessage(this.game.t('roles/town:neapolitanResultVanilla'));
+		} else {
+			this.player.queueMessage(this.game.t('roles/town:neapolitanResultNotVanilla'));
+		}
+	}
+}
