465d345b4ca16629a00efe56bb2ea76413e92698
Radiating8 <theradiatingone@gmail.com>
2021-05-15 14:14:03 -0400

    Add watch action

---
+++ src/lib/mafia/actions/common/WatchAction.ts
@@ -0,0 +1,24 @@
+import NightActionsManager, { NightActionPriority } from '../../managers/NightActionsManager';
+import type { ActionRole } from '../../structures/ActionRole';
+import type Player from '../../structures/Player';
+import { SingleTargetAction } from '../mixins/SingleTargetAction';
+
+// Action to return all players that visited a target.
+export class WatchAction extends SingleTargetAction {
+	public name = 'watch';
+	public priority = NightActionPriority.Investigative;
+	public constructor(role: ActionRole, public vests = 1) {
+		super(role);
+		this.actionText = this.game.t('roles/actions:watchText');
+		this.actionGerund = this.game.t('roles/actions:watchGerund');
+	}
+
+	public tearDown(actions: NightActionsManager, target: Player) {
+		const visitors = target.visitors.filter((visitor) => visitor !== this.player);
+		if (visitors.length > 0)
+			return this.player.queueMessage(
+				this.game.t('roles/town:lookoutVisitedBy', { players: visitors.map((visitor) => visitor.user.username) })
+			);
+		return this.player.queueMessage(this.game.t('roles/town:lookoutNoVisits'));
+	}
+}
