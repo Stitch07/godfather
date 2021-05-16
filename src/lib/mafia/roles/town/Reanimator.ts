2f127e2c3e379ba7cff816f2156009d13e3e1e94
Stitch07 <soumildiscord@gmail.com>
2021-05-16 13:05:04 +0530

    convert all remaining Town roles, add DoubleTargetAction

--- src/lib/mafia/roles/town/Reanimator.ts
+++ src/lib/mafia/roles/town/Reanimator.ts
@@ -1,20 +1,41 @@
 import NightActionsManager, { NightActionPriority } from '@mafia/managers/NightActionsManager';
-import DoubleTarget from '@mafia/mixins/DoubleTarget';
 import Townie from '@mafia/mixins/Townie';
 import type Player from '@mafia/structures/Player';
 import type SingleTarget from '../../mixins/SingleTarget';
 import { Phase } from '@mafia/structures/Game';
+import { ActionRole } from '../../structures/ActionRole';
+import { DoubleTargetAction } from '../../actions/mixins/DoubleTargetAction';
+import type { NightAction } from '../../managers/NightAction';
 
 const INVALID_ROLES = ['Transporter', 'Reanimator', 'Veteran', 'Necromancer'];
 
-class Reanimator extends DoubleTarget {
+class Reanimator extends ActionRole {
 	public name = 'Reanimator';
-	public action = 'reanimate';
-	public priority = NightActionPriority.Reanimator;
+	public actions: NightAction[] = [new ReanimateAction(this)];
 
 	public constructor(player: Player) {
 		super(player);
 		this.description = this.game.t('roles/town:reanimatorDescription');
+	}
+
+	public canUseAction() {
+		const validTargets = this.game.players.filter((player) => this.actions[0].canUse([player]).check);
+		return { check: validTargets.length > 0, reason: this.game.t('roles/global:noTargets') };
+	}
+
+	public static unique = true;
+}
+
+Reanimator.categories = [...Reanimator.categories, 'Town Support'];
+
+export default Townie(Reanimator);
+
+export class ReanimateAction extends DoubleTargetAction {
+	public action = 'reanimate';
+	public priority = NightActionPriority.Reanimator;
+
+	public constructor(role: ActionRole) {
+		super(role);
 		this.actionText = this.game.t('roles/actions:reanimatorText');
 		this.actionGerund = this.game.t('roles/actions:reanimatorGerund');
 	}
@@ -34,12 +55,7 @@ class Reanimator extends DoubleTarget {
 		await (actor.role as SingleTarget).tearDown.call(thisArg, actions, target);
 	}
 
-	public canUseAction() {
-		const validTargets = this.game.players.filter((player) => this.canTarget([player]).check);
-		return { check: validTargets.length > 0, reason: this.game.t('roles/global:noTargets') };
-	}
-
-	public canTarget([player]: Player[]) {
+	public canUse([player]: Player[]) {
 		if (player.isAlive) return { check: false, reason: this.game.t('roles/town:reanimatorDeadOnly') };
 		if (!Reflect.has(player.role, 'action') || Reflect.get(player.role, 'actionPhase') !== Phase.Night)
 			return { check: false, reason: this.game.t('roles/town:reanimatorActionOnly') };
@@ -49,13 +65,7 @@ class Reanimator extends DoubleTarget {
 		return { check: true, reason: '' };
 	}
 
-	public actionConfirmation([player]: Player[]) {
+	public confirmation([player]: Player[]) {
 		return this.game.t('roles/town:reanimatorActionConfirmation', { target: player });
 	}
-
-	public static unique = true;
 }
-
-Reanimator.categories = [...Reanimator.categories, 'Town Support'];
-
-export default Townie(Reanimator);
